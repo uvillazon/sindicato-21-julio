b71c26eaa66cf09c3fa32b8df63ae0bfbc2d1010
Ubaldo Francisco Villazon Villca <ubaldo.villazon@elfec.bo>
2015-06-17 16:07:25 -0400

    Ignorando algunos archivos

---
+++ branches/Sindicato/Sindicato.Services/Interfaces/IDescuentosServices.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sindicato.Common;
+using Sindicato.Model;
+using Sindicato.Services.Model;
+using System.Linq.Expressions;
+
+namespace Sindicato.Services.Interfaces
+{
+    public interface IDescuentosServices
+    {
+
+        IEnumerable<SD_DESCUENTOS> ObtenerDescuentosPaginados(PagingInfo paginacion, FiltrosModel<OtrosModel> filtros);
+        IEnumerable<SD_DESCUENTOS_SOCIO> ObtenerDetalleDescuentos(PagingInfo paginacion, FiltrosModel<OtrosModel> filtros);
+        
+        RespuestaSP GuardarCierre(SD_CIERRES cierre, string login);
+
+        SD_CIERRES ObtenerUltimoRegistroCierre();
+        
+    }
+}
---
+++ branches/Sindicato/Sindicato.Services/Model/OtrosModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sindicato.Services.Model
+{
+    public class OtrosModel
+    {
+        public int? ID_CIERRE { get; set; }
+        public int? ID_DESCUENTO { get; set; }
+        public int? ID_SOCIO { get; set; }
+    }
+}
