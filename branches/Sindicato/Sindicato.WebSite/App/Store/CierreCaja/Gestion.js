6ee7432567ec4435618124d53f6c49ac9db2681a
Ubaldo villazon <uvillazon@gmail.com>
2020-11-11 16:25:37 -0400

    mejoras

---
+++ branches/Sindicato/Sindicato.WebSite/App/Store/CierreCaja/Gestion.js
@@ -0,0 +1,6 @@
+﻿Ext.define('App.Store.CierreCaja.Gestion', {
+    extend: 'App.Config.Abstract.Store',
+    model: 'App.Model.CierreCaja.Gestion',
+    url: 'Cierres/ObtenerGestionesPaginados',
+    sortProperty: 'ID'
+});
